401c5e8a188eaef78e4de4fb329ba704da3e94d1
Jose Santamargarita <josant83@hotmail.es>
2026-01-15 22:44:37 +0100

    first commit

---
+++ app/Controllers/News.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Exceptions\PageNotFoundException;
+use App\Models\NewsModel;
+
+class News extends BaseController {
+    public function index() {
+        $model = model(NewsModel::class);
+
+        $data = [
+            'news_list' => $model->getNews(),
+            'title' => 'News archive'
+        ];
+
+        return view ('templates/header', $data)
+            . view('news/index')
+            . view ('templates/footer');
+    }
+
+    public function show($slug = null) {
+        $model = model(NewsModel::class);
+
+        $data['news'] = $model->getNews($slug);
+
+        if ($data['news'] === null) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        return view ('templates/header', $data)
+            . view('news/view')
+            . view ('templates/footer');
+    }
+}
